7ee68b1dfb6ec71944fb9e6a98e3d86be7e6b3ad
FelipeViacava <fevifrei@hotmail.com>
2023-07-02 23:40:41 -0300

    changed app to streamlit and included cv file

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+# ----- IMPORTS ----- #
+import streamlit as st
+from pathlib import Path
+from PIL import Image
 
-app = Flask(__name__)
+# ----- PATH SETTINGS ----- #
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assets" / "cv.pdf"
+profile_pic = current_dir / "assets" / "profile-pic.png"
 
-@app.route('/')
-def home():
-    return render_template('resume.html')
+# ----- GENERAL SETTINGS ----- #
+PAGE_TITLE = "Felipe Viacava | Digital CV"
+PAGE_ICON = ":wave:"
+NAME = "Felipe Viacava"
+DESCRIPTION = """
+Credit Risk Analyst at Santander and Data Science Graduate Student at Insper
+São Paulo, Brazil
+"""
+EMAIL = "felipeviacava1@gmail.com"
+SOCIAL_MEDIA = {
+    "GitHub": "https://github.com/FelipeViacava",
+    "Linkedin": "https://linkedin.com/in/FelipeViacava",
+}
+PROJECTS = {
+    "⚽🎮 EA Sports FIFA player suggestion system using Shiny Apps": "https://felipeviacava.shinyapps.io/Scout-Buddy/"    
+}
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# ----- STREAMLIT PAGE CONFIGURATION ----- #
+st.set_page_config(page_title=PAGE_TITLE,page_icon=PAGE_ICON)
+st.title("Hello, stranger!")
+
+# ----- STYLING AND ASSETS ---- #
+with open(css_file) as f:
+    st.markdown("<style>{}<style>".format(f.read()), unsafe_allow_html=True)
+
+with open(resume_file,"rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+profile_pic = Image.open(profile_pic)
